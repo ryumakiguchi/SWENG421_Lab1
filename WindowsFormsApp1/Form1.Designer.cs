0a0335b8f9bdf3e97e78b1be17c23594f6e471df
Razer <Razer@LAPTOP-5ERHO4V1>
2024-01-18 18:11:09 -0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLine = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLine
+            // 
+            this.buttonLine.Location = new System.Drawing.Point(40, 46);
+            this.buttonLine.Name = "buttonLine";
+            this.buttonLine.Size = new System.Drawing.Size(163, 65);
+            this.buttonLine.TabIndex = 0;
+            this.buttonLine.Text = "Line";
+            this.buttonLine.UseVisualStyleBackColor = true;
+            this.buttonLine.Click += new System.EventHandler(this.buttonLine_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ellipse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Red;
+            this.trackBar1.Location = new System.Drawing.Point(694, 17);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(189, 114);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.Color.Lime;
+            this.trackBar2.Location = new System.Drawing.Point(889, 17);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(204, 114);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(13, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1572, 857);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.BackColor = System.Drawing.Color.Blue;
+            this.trackBar3.Location = new System.Drawing.Point(1089, 17);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(204, 114);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            this.trackBar3.ValueChanged += new System.EventHandler(this.trackBar3_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(458, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 65);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Rectangle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1597, 1006);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonLine);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLine;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private enum DrawingShape { Line, Rectangle, Ellipse }
+        private DrawingShape currentShape = DrawingShape.Line;
+
+        private Point startPoint;
+        private Color currentColor;
+        private List<Shape> shapes = new List<Shape>();
+        private Bitmap offScreenBitmap;
+        private Graphics offScreenGraphics;
+        public Form1()
+        {
+            InitializeComponent();
+            currentColor = Color.Red;
+            InitializeOffScreenBuffer();
+        }
+        private void InitializeOffScreenBuffer()
+        {
+            offScreenBitmap = new Bitmap(panel1.Width, panel1.Height);
+            offScreenGraphics = Graphics.FromImage(offScreenBitmap);
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Save the drawn shape to the list
+            shapes.Add(CreateShape(startPoint, e.Location));
+
+            // Redraw all shapes on the off-screen buffer
+            offScreenGraphics.Clear(panel1.BackColor);
+            Pen pen = new Pen(currentColor);
+            foreach (Shape shape in shapes)
+            {
+                shape.Draw(offScreenGraphics, pen);
+            }
+
+            panel1.Invalidate(); // Trigger paint event to copy the off-screen buffer to the screen
+        }
+        private void buttonLine_Click(object sender, EventArgs e)
+        {
+            currentShape = DrawingShape.Line;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateColor();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = e.Location;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentShape = DrawingShape.Rectangle;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentShape = DrawingShape.Ellipse;
+        }
+
+        private void UpdateColor()
+        {
+            int red = Math.Min(255, Math.Max(0, trackBar1.Value));
+            int green = Math.Min(255, Math.Max(0, trackBar2.Value));
+            int blue = Math.Min(255, Math.Max(0, trackBar3.Value));
+
+            Console.WriteLine($"Updating color: R={red}, G={green}, B={blue}");
+
+            currentColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateColor();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Draw on the off-screen buffer
+                offScreenGraphics.Clear(panel1.BackColor);
+
+                Pen previewPen = new Pen(currentColor);
+
+                // Draw all shapes
+                foreach (Shape shape in shapes)
+                {
+                    shape.Draw(offScreenGraphics, previewPen);
+                }
+
+                // Draw the preview shape
+                switch (currentShape)
+                {
+                    case DrawingShape.Line:
+                        offScreenGraphics.DrawLine(previewPen, startPoint, e.Location);
+                        break;
+                    case DrawingShape.Rectangle:
+                        offScreenGraphics.DrawRectangle(previewPen, Math.Min(startPoint.X, e.X), Math.Min(startPoint.Y, e.Y), Math.Abs(e.X - startPoint.X), Math.Abs(e.Y - startPoint.Y));
+                        break;
+                    case DrawingShape.Ellipse:
+                        offScreenGraphics.DrawEllipse(previewPen, Math.Min(startPoint.X, e.X), Math.Min(startPoint.Y, e.Y), Math.Abs(e.X - startPoint.X), Math.Abs(e.Y - startPoint.Y));
+                        break;
+                }
+
+                panel1.Invalidate(); // Trigger paint event to redraw the preview
+            }
+        }
+
+        private void trackBar3_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateColor();
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(offScreenBitmap, 0, 0);
+        }
+
+        private abstract class Shape
+        {
+            public abstract void Draw(Graphics g, Pen pen);
+        }
+
+        // Class for a line
+        private class Line : Shape
+        {
+            public Point StartPoint { get; set; }
+            public Point EndPoint { get; set; }
+
+            public override void Draw(Graphics g, Pen pen)
+            {
+                g.DrawLine(pen, StartPoint, EndPoint);
+            }
+        }
+
+        // Class for a rectangle
+        private class RectangleShape : Shape
+        {
+            public Rectangle Bounds { get; set; }
+
+            public override void Draw(Graphics g, Pen pen)
+            {
+                g.DrawRectangle(pen, Bounds);
+            }
+        }
+
+        // Class for an ellipse
+        private class Ellipse : Shape
+        {
+            public Rectangle Bounds { get; set; }
+
+            public override void Draw(Graphics g, Pen pen)
+            {
+                g.DrawEllipse(pen, Bounds);
+            }
+        }
+
+        // Create a shape based on the current shape type
+        private Shape CreateShape(Point start, Point end)
+        {
+            switch (currentShape)
+            {
+                case DrawingShape.Line:
+                    return new Line { StartPoint = start, EndPoint = end };
+                case DrawingShape.Rectangle:
+                    return new RectangleShape { Bounds = new Rectangle(Math.Min(start.X, end.X), Math.Min(start.Y, end.Y), Math.Abs(end.X - start.X), Math.Abs(end.Y - start.Y)) };
+                case DrawingShape.Ellipse:
+                    return new Ellipse { Bounds = new Rectangle(Math.Min(start.X, end.X), Math.Min(start.Y, end.Y), Math.Abs(end.X - start.X), Math.Abs(end.Y - start.Y)) };
+                default:
+                    return null;
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
